96f24bbeb6bc08be0c530b193cfa3544962dbedb
VIDIT-13 <viditallen4@gmail.com>
2025-12-17 12:18:59 +0530

    updated

--- client/src/libs/axios.js
+++ client/src/libs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL= import.meta.env.MODE==="development" ?"http://localhost:5002/api" :"/api"
+const BASE_URL= "https://chattrix-backend.onrender.com"
 
 const axiosInstance= axios.create({
   baseURL: BASE_URL,
